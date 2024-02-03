f53873f1254530c242c452d72c65872b23bd26e6
satnav19 <121519099+satnav19@users.noreply.github.com>
2024-02-03 22:57:21 +0200

    complete overhaul

--- hw1.cpp
+++ hw1.cpp
@@ -1,48 +1,287 @@
 #include "tokens.hpp"
-#include <stdio.h>
-void printer(int token){
-  switch (token) {
-    case ERR: printf("ERR %s\n",yytext);break;
-    case WS: return;
-    case VOID: printf("%d VOID %s\n", yylineno, yytext);break;
-    case INT: printf("%d INT %s\n", yylineno, yytext);break;
-    case BYTE: printf("%d BYTE %s\n", yylineno, yytext);break;
-    case B: printf("%d B %s\n", yylineno, yytext);break;
-    case BOOL: printf("%d BOOL %s\n", yylineno, yytext);break;
-    case AND: printf("%d AND %s\n", yylineno, yytext);break;
-    case OR: printf("%d OR %s\n", yylineno, yytext);break;
-    case NOT: printf("%d NOT %s\n", yylineno, yytext);break;
-    case TRUE: printf("%d TRUE %s\n", yylineno, yytext);break;
-    case FALSE: printf("%d FALSE %s\n", yylineno, yytext);break;
-    case RETURN: printf("%d RETURN %s\n", yylineno, yytext);break;
-    case IF: printf("%d IF %s\n", yylineno, yytext);break;
-    case ELSE: printf("%d ELSE %s\n", yylineno, yytext);break;
-    case WHILE: printf("%d WHILE %s\n", yylineno, yytext);break;
-    case BREAK: printf("%d BREAK %s\n", yylineno, yytext);break;
-    case CONTINUE: printf("%d CONTINUE %s\n", yylineno, yytext);break;
-    case SC: printf("%d SC %s\n", yylineno, yytext);break;
-    case LPAREN: printf("%d LPAREN %s\n", yylineno, yytext);break;
-    case RPAREN: printf("%d RPAREN %s\n", yylineno, yytext);break;
-    case LBRACE: printf("%d LBRACE %s\n", yylineno, yytext);break;
-    case RBRACE: printf("%d RBRACE %s\n", yylineno, yytext);break;
-    case ASSIGN: printf("%d ASSIGN %s\n", yylineno, yytext);break;
-    case RELOP: printf("%d RELOP %s\n", yylineno, yytext);break;
-    case BINOP: printf("%d BINOP %s\n", yylineno, yytext);break;
-    case COMMENT: printf("%d COMMENT //\n", yylineno);break;
-    case ID: printf("%d ID %s\n", yylineno, yytext);break;
-    case NUM: printf("%d NUM %s\n", yylineno, yytext);break;
-    case STRING: printf("%d STRING %s", yylineno, yytext);break;
-    case INNERSTRING: printf("kaki %s", yytext);
-    default: printf("%d WTF %s\n", yylineno, yytext);break;
+#include <iostream>
+using namespace std;
+const int maxLength = 1030;
+bool notInRange(char current, char lower, char upper) {
+	return ((current < lower) || (upper < current));
+}
+
+bool printable(char c)
+{
+	return notInRange(c, char(0x0), char(0x1f)) && int(c) != 0x7f;
+}
+
+
+bool validEsc(char next)
+{
+	return (next == 'n' 
+      || next == 'r' 
+      || next == '0' ||
+      next == 't' ||
+      next == '\\' ||
+      next == '\"' ||
+      next == 'x');
+}
+int toInt(char c)
+{
+	int result = 0;
+	if (c >= '0' && c <= '9') {
+		result =  (int)(c - '0');
+	}
+	else if (c >= 'a' && c <= 'f') {
+		result = (int)(c - 'a' + 10);
+	}
+	else if (c >= 'A' && c <= 'F') {
+		result =  (int)(c - 'A' + 10);
+	}
+	return result;
+}
+
+void addEscSeq(char* text, int* index, int position)
+{
+	(*index)++;
+	char next_char = yytext[*index];
+	switch (next_char)
+	{	
+		case '\\':
+			text[position] = '\\';
+			break;
+			
+		case '"':
+			text[position] = '\"';
+			break;
+			
+		case 'n':
+			text[position] = '\n';
+			break;
+			
+		case 'r':
+			text[position] = '\r';
+			break;
+			
+		case 't':
+			text[position] = '\t';
+			break;
+			
+		case '0':
+			text[position] = '\0'; 
+			break;
+			
+		case 'x':
+			(*index)++;
+			int hexadecimal = toInt(yytext[*index]) * 16;
+			(*index)++;
+			hexadecimal += toInt(yytext[*index]);
+			text[position] = (char)hexadecimal;
+			break;
+	}
+}
+void printer(const char* token_name, const char* token_value)
+{
+	cout << yylineno << " " << token_name << " " << token_value << endl;
+}
+
+
+void renderText()
+{
+	char textArr[maxLength] = {0};
+	int current_write_index = 0;
+
+	for(int i = 1; i < yyleng - 1; i++)
+	{
+		if (yytext[i] == '\\') {
+			addEscSeq(textArr, &i, current_write_index);
+		}
+		else {
+			textArr[current_write_index] = yytext[i];
+		}
+		current_write_index++;
+	}
+
+	cout << yylineno << " " << "STRING" << " " << textArr << endl;
+}
+
+void stringPolice()
+{
+	for (int index = 0; index < yyleng ; index++)
+	{
+		if (!printable(yytext[index])) {
+			cout << "Error " << yytext[index] << endl;
+			return;
+		}
+
+		if (yytext[index] != '\\') {
+			continue;
+		}
+
+		if (index + 1 == yyleng) {
+			cout << "Error unclosed string" << endl;
+			return;
+		}
+		
+		char escaped = yytext[index + 1];
+		if (!validEsc(escaped)) {
+			cout << "Error undefined escape sequence " << escaped << endl;
+			return;
+		}
+		
+		if (escaped == 'x')
+		{ 
+			if (!(index + 2 < yyleng - 1)) {
+				cout << "Error undefined escape sequence x" << endl;
+				return;
+			}
+
+			if (!(index + 3 < yyleng - 1)) {
+				cout << "Error undefined escape sequence x" << yytext[index + 2] << endl;
+				return;
+			}
+
+			if ((notInRange(yytext[index + 2], '0', '7')) ||
+				(notInRange(yytext[index + 3], '0', '9') &&
+			     notInRange(yytext[index + 3], 'a', 'f') &&
+				 notInRange(yytext[index + 3], 'A', 'F'))){
+				cout << "Error undefined escape sequence x" <<
+        yytext[index + 2] << yytext[index + 3] << endl;
+				return;
+			}
+			index += 2;			
+		}
+	}
+}
+
+void errorHandler(tokentype err)
+{
+	int index = 0;
+	switch (err)
+	{
+	case UNCLOSED:
+		cout << "Error unclosed string" << endl;
+		exit(0);
+	case ERR:
+		
+		while (yytext[index])
+		{
+			if (!printable(yytext[index])) {
+				cout << "Error " << yytext[index] << endl;
+				exit(0);
+			}
+			index++;
+		}
+
+		cout << "Error " << yytext << endl;
+		exit(0);
+	case BAD_STRING:
+		stringPolice();
+		exit(0);
+	
+  default:
+    cout << "BAD " << yytext << endl;
+    exit(0);
+  }
   }
 
-}
 int main()
 {
 	int token;
-	while (token=yylex()){
-  printer(token);
-  }	
-  return 0;
+	while ((token = yylex())) 
+	{
+	  switch (token)
+	  {
+	  	case VOID:
+			printer("VOID", yytext);
+			break;
+		case INT:
+			printer("INT", yytext);
+			break;
+		case BYTE:
+			printer("BYTE", yytext);
+			break;
+		case B:
+			printer("B", yytext);
+			break;
+		case BOOL:
+			printer("BOOL", yytext);
+			break;
+		case AND:
+			printer("AND", yytext);
+			break;
+		case OR:
+			printer("OR", yytext);
+			break;
+		case NOT:
+			printer("NOT", yytext);
+			break;
+		case TRUE:
+			printer("TRUE", yytext);
+			break;
+		case FALSE:
+			printer("FALSE", yytext);
+			break;
+		case RETURN:
+			printer("RETURN", yytext);
+			break;
+		case IF:
+			printer("IF", yytext);
+			break;
+		case ELSE:
+			printer("ELSE", yytext);
+			break;
+		case WHILE:
+			printer("WHILE", yytext);
+			break;
+		case BREAK:
+			printer("BREAK", yytext);
+			break;	
+		case CONTINUE:
+			printer("CONTINUE", yytext);
+			break;
+		case SC:
+			printer("SC", yytext);
+			break;
+		case LPAREN:
+			printer("LPAREN", yytext);
+			break;
+		case RPAREN:
+			printer("RPAREN", yytext);
+			break;
+		case LBRACE:
+			printer("LBRACE", yytext);
+			break;
+		case RBRACE:
+			printer("RBRACE", yytext);
+			break;
+		case ASSIGN:
+			printer("ASSIGN", yytext);
+			break;
+		case RELOP:
+			printer("RELOP", yytext);
+			break;
+		case BINOP:
+			printer("BINOP", yytext);
+			break;
+		case COMMENT:
+			printer("COMMENT", "//");
+			break;
+		case ID:
+			printer("ID", yytext);
+			break;
+		case NUM:
+			printer("NUM", yytext);
+			break;
+		case STRING:
+			renderText();
+			break;
+		case BAD_STRING:
+			errorHandler(BAD_STRING);
+			exit(0);
+		case UNCLOSED:
+			errorHandler(UNCLOSED);
+			exit(0);
+		case ERR:
+			errorHandler(ERR);
+			exit(0);
+	  }
+	}
+	return 0;
 }
-
